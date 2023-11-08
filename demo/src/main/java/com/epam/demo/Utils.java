052078e2da8152c3b715a51d9c03673955918382
Welkus <vdelkus@gmail.com>
2023-11-08 15:55:53 +0200

    Task Done?

--- demo/src/main/java/com/epam/demo/Utils.java
+++ demo/src/main/java/com/epam/demo/Utils.java
@@ -1,9 +1,12 @@
 package com.epam.demo;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class Utils {
     public static boolean isAllPositiveNumbers(List<String> args) {
-       //magic happens here
+        if(args == null) return false;
+
+        return   args.stream().noneMatch(n -> n.startsWith("0") || n.startsWith("-") || n.isEmpty() || n.contains(" ") );
     }
 }
--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -2,6 +2,16 @@ package com.epam.utils;
 
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
-        //code!
+        try {
+            if(str.startsWith("0")){
+                return false;
+            }
+            return Double.parseDouble(str) > 0;
+
+        } catch (NullPointerException e) {
+            return false;
+        } catch (NumberFormatException e) {
+            return false;
+        }
     }
 }
